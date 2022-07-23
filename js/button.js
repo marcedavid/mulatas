2ea2d77d1e5b941d4af12c6c34aca134679639e3
Marce David <marcelodavid.95@gmail.com>
2022-07-22 23:00:30 -0300

    added styled animation with js

---
+++ js/button.js
@@ -0,0 +1,20 @@
+const btnBuy = document.querySelectorAll('.btn-buy');
+const geeks = document.querySelectorAll('.geeks'); 
+
+for (let i = 0; i < geeks.length; i++) {
+  geeks[i].addEventListener("mouseover", function() {      
+      console.log('entrando')  
+      btnBuy[i].style.opacity = '1';
+      //btnBuy[i].classList.toggle("red");
+  
+  });
+}
+
+for (let i = 0; i < geeks.length; i++) {
+  geeks[i].addEventListener("mouseleave", function() {      
+      console.log('entrando')  
+      btnBuy[i].style.opacity = '0';
+      //btnBuy[i].classList.toggle("red");
+  
+  });
+}
