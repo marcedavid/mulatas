bf13e242d67a181b8c99e028227d53aa7ebf2b69
Marce David <marcelodavid.95@gmail.com>
2022-07-26 12:17:58 -0300

    added an event to replace a icon cart if cart have products inside

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector('#mySidenav');
 const contenedorCarrito = document.querySelector('#list-cart tbody');
 const listProd = document.querySelector('#listProd');
 const cartCount = document.querySelector('.cart-count');
+const cartIcon = document.querySelector('.icon-cart');
 let artCart = [];
 let totalCount = 0;
 
@@ -40,6 +41,7 @@ function removeProd(e){
       artCart = artCart.filter(prod => prod.id !== prodId);
         if(prodId){
           cartCount.textContent = artCart.length
+          cartIcon.setAttribute('d','M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z')
         }
       carritoHTML();
 
@@ -71,6 +73,8 @@ function leerDatos(prod){
         }else{
             artCart = [...artCart, infoProds];
             cartCount.textContent = artCart.length;
+            cartIcon.setAttribute('d','M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z');
+            
         }
   if( artCart !== 0 ){
     textCart.style.visibility = 'hidden';
